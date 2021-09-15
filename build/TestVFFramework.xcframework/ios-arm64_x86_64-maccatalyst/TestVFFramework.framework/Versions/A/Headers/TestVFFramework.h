a57f8b7a6345c837cd051da40e2b0890550e754e
satyendraNeosoft <90760824+satyendraNeosoft@users.noreply.github.com>
2021-09-15 16:19:11 +0530

    Initial V1.0.1

---
+++ build/TestVFFramework.xcframework/ios-arm64_x86_64-maccatalyst/TestVFFramework.framework/Versions/A/Headers/TestVFFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestVFFramework.h
+//  TestVFFramework
+//
+//  Created by Neosoft on 15/09/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestVFFramework.
+FOUNDATION_EXPORT double TestVFFrameworkVersionNumber;
+
+//! Project version string for TestVFFramework.
+FOUNDATION_EXPORT const unsigned char TestVFFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestVFFramework/PublicHeader.h>
+
+
